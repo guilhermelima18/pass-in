76e7180e80972f1d1558315b33ddb36bb0d77fd9
guilhermelimaflex <guilherme.lima@flexconsulta.com.br>
2024-04-01 21:30:37 -0300

    config fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,28 @@
 import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import "../styles/global.css";
 
 export default function Layout() {
-  return <Slot />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <></>;
+  }
+
+  return (
+    <>
+      <StatusBar style="light" />
+      <Slot />
+    </>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View className="bg-red-500">
-      <Text>Pass.in</Text>
+    <View className="bg-green-500 flex-1 items-center justify-center">
+      <Text className="text-white">Pass.in</Text>
     </View>
   );
 }
